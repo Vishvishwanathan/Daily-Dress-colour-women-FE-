b93f48c1e01cb7e15bdc8076acd0f2170e726da7
Vishvishwanathan <vishvishva204@gmail.com>
2024-03-12 09:35:51 +0530

    new commet

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,21 +11,23 @@ import {
   faEnvelope,
   faHouse,
 } from "@fortawesome/free-solid-svg-icons";
-// const signin = {};
-// const register = {};
-// const b1 = {};
-// const pass = {};
-// const pass1 = {};
-// const value = "" ;
-// const value1 = "" ;
-// const value2 = "" ;
-// const value3 = "" ;
+const signin = {};
+const register = {};
+const b1 = {};
+const pass = {};
+const pass1 = {};
+const value = "";
+const value1 = "";
+const value2 = "";
+const value3 = "";
 
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
   let [firstName, setFirstName] = useState("");
   let [lastName, setLastName] = useState("");
+  let [isLogin, setisLogin] = useState(false);
+
   let navigate = useNavigate();
 
 
@@ -47,7 +49,7 @@ function Login() {
     } catch (error) {
       toast.error(
         error.response.data.message ||
-          "Error Occoured! Please try after some time"
+        "Error Occoured! Please try after some time"
       );
     }
   };
@@ -68,7 +70,7 @@ function Login() {
     } catch (error) {
       toast.error(
         error.response.data.message ||
-          "Error Occoured! Please try after some time" 
+        "Error Occoured! Please try after some time"
       );
     }
   };
@@ -93,20 +95,13 @@ function Login() {
   };
 
   let logintoggle = () => {
-    signin.style.left = "50px";
-    register.style.left = "250px";
-    b1.style.background = "#ffffff !important";
-    value1.value = "";
-    value2.value = "";
-    value3.value = "";
-    pass1.value = "";
+    setisLogin(true)
   };
   let registertoggle = () => {
-    register.style.left = "-200px";
-    signin.style.left = "-300px";
-    value.value = "";
-    pass.value = "";
+    setisLogin(false)
+
   };
+
   return (
     <>
       <div className="home">
@@ -126,160 +121,162 @@ function Login() {
           </div>
           <div className="container d-flex flex-row">
             {/* <!-- login In Form --> */}
-            <Form className="input-group" id="signin">
-              <span className="text">Welcome Back</span>
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                style={{
-                  color: "#ffffff",
-                  marginTop: "65px",
-                  marginLeft: "225px",
-                  position: "absolute",
-                }}
-              />
-              <input
-                type="text"
-                className="input-field"
-                id="value"
-                placeholder="Email"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <FontAwesomeIcon
-                icon={faEye}
-                style={{
-                  color: "#fcfcfc",
-                  cursor: "pointer",
-                  marginLeft: "225px",
-                  marginTop: "130px",
-                  position: "absolute",
-                }}
-                onMouseOver={() => {
-                  pass.type = "text"; 
-                }}
-                onMouseOut={() => {
-                  pass.type = "password";
-                }}
-              />
-              <input
-                type="password"
-                className="input-field"
-                id="pass"
-                placeholder="Password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button
-                type="submit"
-                className="submit-btn"
-                onClick={(e) => validateLogin(e)}
-              >
-                Login
-              </Button>
-              <Button
-                type="submit"
-                className="submit-btn"
-                onClick={(e) => validatereset(e)}
-              >
-                Forget Password
-              </Button>
-              <Button className="home-btn" onClick={() => navigate("/")}>
-                <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} />
-              </Button>
-            </Form>
+            {
+              isLogin ? <Form className="input-group">
+                <span className="text">Welcome Back</span>
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  style={{
+                    color: "#ffffff",
+                    marginTop: "65px",
+                    marginLeft: "225px",
+                    position: "absolute",
+                  }}
+                />
+                <input
+                  type="text"
+                  className="input-field"
+                  id="value"
+                  placeholder="Email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <FontAwesomeIcon
+                  icon={faEye}
+                  style={{
+                    color: "#fcfcfc",
+                    cursor: "pointer",
+                    marginLeft: "225px",
+                    marginTop: "130px",
+                    position: "absolute",
+                  }}
+                  onMouseOver={() => {
+                    pass.type = "text";
+                  }}
+                  onMouseOut={() => {
+                    pass.type = "password";
+                  }}
+                />
+                <input
+                  type="password"
+                  className="input-field"
+                  id="pass"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                  type="submit"
+                  className="submit-btn"
+                  onClick={(e) => validateLogin(e)}
+                >
+                  Login
+                </Button>
+                <Button
+                  type="submit"
+                  className="submit-btn"
+                  onClick={(e) => validatereset(e)}
+                >
+                  Forget Password
+                </Button>
+                <Button className="home-btn" onClick={() => navigate("/")}>
+                  <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} />
+                </Button>
+              </Form>
+                :
+                < Form className=" input-group" >
+                  <span className="text">Register Now</span>
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    style={{
+                      color: "#ffffff",
+                      marginTop: "46px",
+                      marginLeft: "225px",
+                      position: "absolute",
+                    }}
+                  />
+                  <input
+                    type="text"
+                    className="input-field"
+                    id="value1"
+                    placeholder="First Name"
+                    required
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    style={{
+                      color: "#ffffff",
+                      marginTop: "100px",
+                      marginLeft: "225px",
+                      position: "absolute",
+                    }}
+                  />
+                  <input
+                    type="text"
+                    className="input-field"
+                    id="value2"
+                    placeholder="Last Name"
+                    required
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    style={{
+                      color: "#ffffff",
+                      marginTop: "157px",
+                      marginLeft: "225px",
+                      position: "absolute",
+                    }}
+                  />
+                  <input
+                    type="text"
+                    className="input-field"
+                    id="value3"
+                    placeholder="Email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faEye}
+                    style={{
+                      color: "#fcfcfc",
+                      cursor: "pointer",
+                      marginLeft: "225px",
+                      marginTop: "211px",
+                      position: "absolute",
+                    }}
+                    onMouseOver={() => {
+                      pass1.type = "text";
+                    }}
+                    onMouseOut={() => {
+                      pass1.type = "password";
+                    }}
+                  />
+                  <input
+                    type="password"
+                    className="input-field"
+                    id="pass1"
+                    placeholder="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    type="submit"
+                    className="submit-btn"
+                    onClick={(e) => validateSignUp(e)}
+                  >
+                    Register
+                  </Button>
+                  <Button className="home-btn" onClick={() => navigate("/")}>
+                    <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} />
+                  </Button>
+                </Form>
 
-            {/* <!-- Sign In Form --> */}
-            <Form className=" input-group" id="register">
-              <span className="text">Register Now</span>
-              <FontAwesomeIcon
-                icon={faUser}
-                style={{
-                  color: "#ffffff",
-                  marginTop: "46px",
-                  marginLeft: "225px",
-                  position: "absolute",
-                }}
-              />
-              <input
-                type="text"
-                className="input-field"
-                id="value1"
-                placeholder="First Name"
-                required
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              <FontAwesomeIcon
-                icon={faUser}
-                style={{
-                  color: "#ffffff",
-                  marginTop: "100px",
-                  marginLeft: "225px",
-                  position: "absolute",
-                }}
-              />
-              <input
-                type="text"
-                className="input-field"
-                id="value2"
-                placeholder="Last Name"
-                required
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                style={{
-                  color: "#ffffff",
-                  marginTop: "157px",
-                  marginLeft: "225px",
-                  position: "absolute",
-                }}
-              />
-              <input
-                type="text"
-                className="input-field"
-                id="value3"
-                placeholder="Email"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <FontAwesomeIcon
-                icon={faEye}
-                style={{
-                  color: "#fcfcfc",
-                  cursor: "pointer",
-                  marginLeft: "225px",
-                  marginTop: "211px",
-                  position: "absolute",
-                }}
-                onMouseOver={() => {
-                  pass1.type = "text";
-                }}
-                onMouseOut={() => {
-                  pass1.type = "password";
-                }}
-              />
-              <input
-                type="password"
-                className="input-field"
-                id="pass1"
-                placeholder="password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button
-                type="submit"
-                className="submit-btn"
-                onClick={(e) => validateSignUp(e)}
-              >
-                Register
-              </Button>
-              <Button className="home-btn" onClick={() => navigate("/")}>
-                <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} />
-              </Button>
-            </Form>
+            }
           </div>
         </div>
-      </div>
+      </div >
     </>
   );
 }
